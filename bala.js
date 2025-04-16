fa9e8ee705fcb06f5197a08c1a14eeb593b3035c
vickybala012 <vickybala682@gmail.com>
2025-04-16 11:44:50 +0530

    Add files via upload
    Good

---
+++ bala.js
@@ -0,0 +1,12 @@
+let car={
+    name:'nexon',
+    brand:'tata',
+    price:15
+}
+function User(car){
+    this.name=car;
+    this.brand=car;
+    this.price=car;
+}
+let rolce=new User('rolce')
+console.log('rolce')
